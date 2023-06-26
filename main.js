0d833439776b41f65482895c9860783458362e1e
Sarath O S <sarathos584@gmail.com>
2023-06-26 09:19:37 +0530

    container padding is adjusted for 275px max-width

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-$(document).ready(function(){
-    $('#toggleBtn').on('click',function(){
-       $('.responsive-nav').slideToggle();
-       $('header').toggleClass('nav-open')
-    })
+$(document).ready(function () {
+  $("#toggleBtn").on("click", function () {
+    $(".responsive-nav").slideToggle();
+    $("header").toggleClass("nav-open");
   });
+  $(".sec-nav-item").on("click", function (event) {
+    $(event.target).toggleClass("active");
+  });
+});
